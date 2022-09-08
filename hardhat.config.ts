40ae500cd26a2d25ad06266499180968ba80923d
Lucas Oliveira <olivmath@protonmail.com>
2022-09-08 02:04:08 -0300

    update: config files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,14 @@ import { HardhatUserConfig } from "hardhat/types"
 import "@nomicfoundation/hardhat-toolbox"
 
 const config: HardhatUserConfig = {
-    solidity: "0.8.15"
+    solidity: "0.8.15",
+    networks: {
+        hardhat: {
+            mining: {
+                auto: true,
+                interval: 1
+            }
+        }
+    }
 }
-
 export default config
