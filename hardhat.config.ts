ba67cba5aba67d2b9b563284a86b420cbdbb7397
Lucas Oliveira <olivmath@protonmail.com>
2022-09-12 01:26:52 -0300

    add: ethernal in hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,21 @@
 import { HardhatUserConfig } from "hardhat/types"
 import "@nomicfoundation/hardhat-toolbox"
+import "hardhat-ethernal"
 
-const config: HardhatUserConfig = {
+const config = {
+    ethernal: {
+        email: "",
+        password: ""
+    },
     solidity: "0.8.17",
     networks: {
         hardhat: {
             mining: {
-                auto: true
+                auto: true,
+                interval: 5000
             }
         }
     }
 }
+
 export default config
